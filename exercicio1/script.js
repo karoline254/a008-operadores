3c7a7612fa2166cac4a4b8da978a858f90a6e4c6
karoline254 <karolineexecut@gmail.com>
2022-08-30 12:11:19 -0300

    Exercicio01

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+
+let num01 = Number (prompt("Digite um número par: "))
+console.log(num01 / 2)
+
+// O resultado sempre vai ser positivo
+
+// Caso insira um número ímpar, o resultado será quebrado. EX: 5 / 2 = 2.5
